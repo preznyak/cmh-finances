ed336cfd95c8118400e0fa3a8410f18b7e596fad
Laszlo Preznyak <laszlopreznyak11@gmail.com>
2024-03-13 08:59:53 +0100

    Initial commit

---
+++ src/main/java/hu/preznyak/cmhfinances/CmhFinancesApplication.java
@@ -0,0 +1,13 @@
+package hu.preznyak.cmhfinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmhFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmhFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/preznyak/cmhfinances/CmhFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.preznyak.cmhfinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmhFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
